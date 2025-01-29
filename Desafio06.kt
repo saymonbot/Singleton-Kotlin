1e97cb2d37b0c2161ac58ed45c9ecda8a221eafb
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-01-29 13:15:07 -0300

    Update Desafio06.kt

--- Desafio06.kt
+++ Desafio06.kt
@@ -4,16 +4,21 @@ class User(val id: Int, val name: String)
  * No Kotlin, a declaração de um objeto (por meio da palavra-chave object) 
  * é uma maneira concisa e eficaz de implementar o padrão Singleton.
  */
+ 
 object UserManager {
 
   private val users = mutableListOf<User>()
 
   fun addUser(name: String) {
-    TODO("Implementar a lógica de adicionar um novo usuário na lista mutável $users.")
+    val newUser = User(users.size.toInt(), name)
+    users.add(newUser)
   }
 
   fun listUsers() {
-    TODO("Implementar a impressão dos $users, seguindo o padrão definido no enunciado.")
+    for (i in 0 until users.size.toInt()){
+      var updatedId = users[i].id + 1
+      println("${updatedId} - ${users[i].name}")
+    }
   }
 }
 
@@ -21,7 +26,7 @@ fun main() {
   val quantity = readLine()?.toIntOrNull() ?: 0
 
   for (i in 1..quantity) {
-    val name = readLine() ?: ""
+    val name = readLine().toString() ?: ""
     UserManager.addUser(name)
   }
 
